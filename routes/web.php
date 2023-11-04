18af8f34cbbde2a1133091354354e6583c72f991
Ajung007 <agungrachmadprihanto@gmail.com>
2023-11-04 10:23:27 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/detail', [DetailController::class, 'index'])->name('detail');
 
 
 Route::prefix('admin')->namespace('Admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
+
